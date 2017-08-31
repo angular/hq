07dfb8a07f7aebcf9283504e99d7a36fb5a6a1fb
Rob Wormald <robwormald@gmail.com>
2017-08-31 15:28:43 -0700

    cleanup

--- functions/lib/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ping_1 = require("./utils/ping");
-exports.ping = ping_1.ping;
-const handler_1 = require("./actions/handler");
-exports.dispatch = handler_1.dispatch;
-const webhook_1 = require("./github/webhook");
-exports.githubWebhook = webhook_1.githubWebhook;
--- functions/lib/ping.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-exports.ping = functions.https.onRequest((req, res) => {
-    res.send(JSON.stringify({ pong: Date.now() }));
-});
